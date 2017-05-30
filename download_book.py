85bd18755cbece9e94058fe23ebc74e008d0b861
sepia <treefrog@riseup.net>
2017-05-30 17:02:52 +0000

    add missing subindexing to main loop

--- download_book.py
+++ download_book.py
@@ -15,10 +15,11 @@ soup = fn.download_page(prefix + "js_005.htm")
 book = fn.parse_toc(soup)
 fn.generate_toc(book)
 
+
 for idx in range(0, len(book["url"])):
     print("downloading chapter " +
-          str(book["chapter"]) + ": " + book["chaptertitle"].strip().lower())
-    soup = fn.download_page(book["url"][idx])
+          str(book["chapter"][idx]) + ": " + book["chaptertitle"][idx].strip().lower())
+    soup = fn.download_page(prefix + book["url"][idx])
     [text, footnotes] = fn.parse_chapter(soup)
     print("parsing chapter...")
     fn.generate_chapter(text, footnotes, book, idx)
